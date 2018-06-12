9db86462828eeeecd54b933abcb962255a640822
Adam Powroznik <adampowroznik95@gmail.com>
2018-06-12 18:08:17 +0200

    initial commit

---
+++ CommunicationWithArduino/MainWindow.xaml.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace CommunicationWithArduino
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SerialPortCommunication com1 = new SerialPortCommunication();
+        DispatcherTimer t = new DispatcherTimer();
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            com1.GetAvaiableComPorts(CBComPorts);
+            
+            t.Interval = TimeSpan.FromMilliseconds(1);
+            t.Start();
+            t.Tick += T_Tick;
+        }
+
+        private void T_Tick(object sender, EventArgs e)
+        {
+            if (com1.Connected)
+            {
+                com1.readCommand();
+            }
+        }
+       
+
+
+
+        private void BTconnect_Click(object sender, RoutedEventArgs e)
+        {
+            if (!com1.Connected)
+            {
+                com1.ConnectToArduino(CBComPorts.SelectedItem.ToString());
+                BTconnect.Content = "Disconect";
+                image2.Visibility = Visibility.Visible;
+                image1.Visibility = Visibility.Hidden;
+                StackPanelControl.IsEnabled = true;
+            }
+            else
+            {
+                com1.DisconnectFromArduino();
+                BTconnect.Content = "Connect";
+                image1.Visibility = Visibility.Visible;
+                image2.Visibility = Visibility.Hidden;
+                StackPanelControl.IsEnabled = false;
+            }
+        }
+
+
+        private void CheckBoxDiode_Click(object sender, RoutedEventArgs e)
+        {
+           if (com1.Connected)
+            {
+                if (CheckBoxDiode.IsChecked == true)
+                {
+                    CheckBoxDiode.Content = "POWER ON";
+                    if (CheckBoxPWM.IsChecked == true)
+                    {
+                        com1.SendCommand("PWM1", Slider1.Value.ToString());
+                    }
+                    else
+                    {
+                        com1.SendCommand("LED1", "ON");
+                    }
+                }
+                else
+                {
+                    com1.SendCommand("LED1", "OFF");
+                    CheckBoxDiode.Content = "POWER OFF";
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            button2.Visibility = Visibility.Hidden;
+            CheckBoxMainLight.Visibility = Visibility.Visible;
+        }
+
+        private void CheckBoxMainLight_Click(object sender, RoutedEventArgs e)
+        {
+            if (com1.Connected)
+            {
+                if (CheckBoxMainLight.IsChecked == true)
+                {
+                    com1.SendCommand("ML01", "ON");
+                }
+                else
+                {
+                    com1.SendCommand("ML01", "OFF");
+                }
+            }
+        }
+
+        private void CheckBoxPWM_Click(object sender, RoutedEventArgs e)
+        {
+            if (CheckBoxPWM.IsChecked == true)
+            {
+                com1.SendCommand("PWM1", Slider1.Value.ToString());
+                Slider1.IsEnabled = true;
+            }
+            else
+            {
+                Slider1.IsEnabled = false;
+                if(CheckBoxDiode.IsChecked==true)
+                    com1.SendCommand("LED1", "ON");
+                else
+                    com1.SendCommand("LED1", "OFF");
+            }
+        }
+
+        private void Slider1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if(CheckBoxDiode.IsChecked == true)
+            {
+                    if (CheckBoxPWM.IsChecked == true)
+                    {
+                    com1.SendCommand("PWM1", Slider1.Value.ToString());
+                    Text1.Text = Slider1.Value.ToString();
+                }
+            }
+        }
+    }
+}
---
+++ CommunicationWithArduino/SerialPortCommunication.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace CommunicationWithArduino
+{
+    class SerialPortCommunication
+    {
+        bool connected;
+        SerialPort port1;
+        String[] ports;
+
+        public bool Connected { get { return connected; } }
+
+        public void GetAvaiableComPorts (ComboBox comboBox)
+        {
+            ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                comboBox.Items.Add(port);
+                Console.WriteLine(port);
+                if (ports[0] != null)
+                    comboBox.SelectedItem = ports[0];
+            }
+        }
+
+        public void ConnectToArduino (string portName)
+        {
+            connected = true;
+            port1 = new SerialPort(portName, 9600, Parity.None, 8, StopBits.One);
+            startCommunication();
+        }
+
+        private void startCommunication()
+        {
+            port1.Open();
+            port1.Write(sendCommand("STAR"));
+            Console.WriteLine("Connected");
+        }
+
+        public void readCommand()
+        {
+            if (port1.BytesToRead>=10)
+            {
+                string something = port1.ReadLine();
+                Console.WriteLine(something);
+                
+            }
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="adress"> 0-9 number of device</param>
+        /// <param name="code"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        private string sendCommand(byte adress, string code, string data)
+        {
+            //adress is 0-9 number
+            string command = adress.ToString() + code + data + "\n";
+            return command;
+        }
+        /// <summary>
+        /// This function send code with additional data and default device adress equal 1
+        /// </summary>
+        /// <param name="code"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        private string sendCommand(string code, string data)
+        {
+            string command = 1.ToString() + code + data + "\n";
+            return command;
+        }
+        /// <summary>
+        /// This functrion sends only code with default value of adress equal 1 and no additional data.
+        /// </summary>
+        /// <param name="code"></param>
+        /// <returns></returns>
+        private string sendCommand(string code)
+        {
+            string command = 1.ToString() + code + "\n";
+            return command;
+        }
+        /// <summary>
+        /// This function sends code with adress of device without additional data
+        /// </summary>
+        /// <param name="adress">Adress is 0-9 number of device.</param>
+        /// <param name="code"></param>
+        /// <returns></returns>
+        private string sendCommand(byte adress, string code)
+        {
+            string command = adress.ToString() + code + "\n";
+            return command;
+        }
+
+        public void DisconnectFromArduino()
+        {
+            connected = false;
+            stopCommunication();
+        }
+
+        private void stopCommunication()
+        {
+            port1.Write(sendCommand("STOP"));
+            port1.Close();
+            Console.WriteLine("Disconnected");
+        }
+
+        /// <summary>
+        /// This function sends code with additional data and default adress of device equal 1.
+        /// </summary>
+        /// <param name="code"></param>
+        /// <param name="data"></param>
+        public void SendCommand (string code, string data)
+        {
+            port1.Write(sendCommand(code, data));
+            Console.WriteLine("Kod: " + code + " Dane: " + data);
+        }
+
+        /// <summary>
+        /// This function sends code without additional data and default adress of device equal 1.
+        /// </summary>
+        /// <param name="code"></param>
+        public void SendCommand(string code)
+        {
+            port1.Write(sendCommand(code));
+            Console.WriteLine("Kod: " + code + " Dane: brak");
+        }
+    }
+}
